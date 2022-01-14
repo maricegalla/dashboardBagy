1a0d100301b347182b8580936417df4ffb774fec
maricegalla <maricegalla@gmail.com>
2022-01-14 09:48:21 -0300

    Main board right section implemented

--- dashboard/src/components/MainBoard/index.jsx
+++ dashboard/src/components/MainBoard/index.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
-import { BigChartContainer } from './style';
+import { BigChartContainer, RightSectionContainer } from './style';
+import {ReactComponent as ArrowDown} from 'src/assets/arrowDownBlack.svg'
+
+const leftSectionContent = [
+  { title: 'Loja', highlight: 'Estilo Pri' },
+  { title: 'Mês', highlight: 'Julho' },
+  { title: 'Ano', highlight: '2020' },
+  { title: 'Total de Faturamento', highlight: 'R$ 45.000,00' },
+  { title: 'Análise comparativa', highlight: 'Positivo' },
+];
 
 const MainBoard = () => {
-  return <BigChartContainer>Teste</BigChartContainer>;
+  return (
+    <BigChartContainer>
+      <RightSectionContainer>
+        {leftSectionContent.map((content, index) => {
+          if (content.title === 'Total de Faturamento') {
+            return (
+              <div key={index}>
+                <p>{content.title}</p>
+                <h2>{content.highlight}</h2>
+              </div>
+            );
+          }
+          if (content.title === 'Análise comparativa') {
+            return (
+              <div key={index}>
+                <p>{content.title}</p>
+                <h2 className="green">{content.highlight}</h2>
+              </div>
+            );
+          } else {
+            return (
+              <div key={index}>
+                <p>{content.title}</p>
+                <h2 className="underline">{content.highlight}<ArrowDown/></h2>
+              </div>
+            );
+          }
+        })}
+      </RightSectionContainer>
+    </BigChartContainer>
+  );
 };
 
 export default MainBoard;
--- dashboard/src/components/MainBoard/style.js
+++ dashboard/src/components/MainBoard/style.js
@@ -2,14 +2,55 @@ import styled from 'styled-components';
 
 const BigChartContainer = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
   height: 550px;
   width: 1130px;
   border: 1px solid #e5e5e5;
   border-radius: 5px;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  padding: 5px;
 `;
 
-export { BigChartContainer };
+const RightSectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 340px;
+  height: 100%;
+  border-left: 1px solid #e5e5e5;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    gap: 20px;
+    border-bottom: 1px solid #e5e5e5;
+
+    & > p {
+      font-size: 12px;
+      color: #9fa2b4;
+      font-weight: 500;
+    }
+
+    & > h2 {
+      display: flex;
+      align-items: flex-end;
+      font-size: 24px;
+      color: #252733;
+      font-weight: 600;
+      text-align: center;
+    }
+
+    .underline {
+      text-decoration: underline !important;
+    }
+
+    .green {
+      color: #219653;
+    }
+  }
+`;
+
+export { BigChartContainer, RightSectionContainer };
