78354c5f59df7b10f5a8e402fcf0870a2e5bf43d
maricegalla <maricegalla@gmail.com>
2022-01-15 10:50:24 -0300

    Chart area gradient

--- dashboard/src/components/Chart/index.jsx
+++ dashboard/src/components/Chart/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ChartContentContainer } from './style';
-import { LineChart, Line, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import { AreaChart, Area, YAxis, Tooltip, CartesianGrid } from 'recharts';
 import CustomTooltip from 'src/components/CustomTooltip';
 import api from 'src/services/api';
 
@@ -19,7 +19,7 @@ const Chart = () => {
 
   return (
     <ChartContentContainer>
-      <LineChart
+      <AreaChart
         width={800}
         height={360}
         data={chartData}
@@ -30,6 +30,12 @@ const Chart = () => {
           bottom: 5,
         }}
       >
+        <defs>
+          <linearGradient id="july" x1="0" y1="0" x2="1" y2="0">
+            <stop offset="1.47%" stopColor="#37f3ff" stopOpacity={1} />
+            <stop offset="96.74%" stopColor="#3751FF" stopOpacity={0} />
+          </linearGradient>
+        </defs>
         <YAxis
           orientation="right"
           tick={{ fontSize: '8px', fill: '#9FA2B4' }}
@@ -41,24 +47,27 @@ const Chart = () => {
           dx={-10}
         />
         <Tooltip content={<CustomTooltip />} />
-        <Line
-          type="monotone"
-          dataKey="Julho"
-          stroke="#fc3c8d"
-          activeDot={{ r: 6 }}
-          strokeWidth={2}
-          dot={false}
-        />
-        <Line
+        <Area
           type="monotone"
           dataKey="Junho"
           stroke="#dfe0eb"
           strokeWidth={2}
           dot={false}
           activeDot={false}
+          fill={'#fff'}
+        />
+        <Area
+          type="monotone"
+          dataKey="Julho"
+          stroke="#fc3c8d"
+          activeDot={{ r: 6 }}
+          strokeWidth={2}
+          dot={false}
+          fill="url(#july)"
+          fillOpacity={0.1}
         />
         <CartesianGrid vertical={false} />
-      </LineChart>
+      </AreaChart>
     </ChartContentContainer>
   );
 };
